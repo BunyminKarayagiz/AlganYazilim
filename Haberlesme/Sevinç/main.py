a75a4d9b19260495e9fce5f7b78ffcc3ece8cfb7
sevinc26 <139468130+sevinc26@users.noreply.github.com>
2023-12-15 15:44:02 +0300

    MAVProxy komutları OOP ile yeniden yazıldı

--- Haberlesme/Sevinç/main.py
+++ Haberlesme/Sevinç/main.py
@@ -1,15 +1,20 @@
-import argparse
-import path
-import time
-try:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--connect', default='tcp:127.0.0.1:5762')
-    args = parser.parse_args()
-    connection_string = args.connect
-    iha=path.Plane(connection_string)
-    while True:
-        print(iha.pos_alt_rel)
-        time.sleep(1)
-except KeyboardInterrupt as e:
-    print(e)
-    pass
+import sympy as sp
+
+fon_expr = '(z+1)/(z**2+2*z)'
+z = sp.Symbol('z')
+fon = sp.sympify(fon_expr)
+a = sp.Symbol("-2")
+n = sp.Symbol('n')
+
+limit_al = (z - a)**n * fon
+
+# Simplify the expression before taking the limit
+limit_al_simplified = sp.simplify(limit_al)
+
+# Calculate the limit
+R = sp.limit(limit_al_simplified, z, a) / sp.factorial(n-1)
+
+# n'i 1 azaltarak faktöriyeli hesapla
+R = R.subs(n, n-1)
+
+print(R)
