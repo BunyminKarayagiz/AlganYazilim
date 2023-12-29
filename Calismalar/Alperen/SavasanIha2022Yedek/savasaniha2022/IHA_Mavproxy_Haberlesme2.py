28e87300de86b8ad7b5fab749ef132aa7e18d66b
AlperenUgur <alperenugur4@gmail.com>
2023-12-29 12:43:41 +0300

    SavasanIha2022 yedek eklendi

---
+++ Calismalar/Alperen/SavasanIha2022Yedek/savasaniha2022/IHA_Mavproxy_Haberlesme2.py
@@ -0,0 +1,179 @@
+from multiprocessing.pool import ThreadPool
+import path
+import time
+import argparse
+import cv2
+import hesaplamalar, haberlesme
+import json
+from dronekit import LocationGlobalRelative
+import datetime
+import ast
+import os
+
+host = "169.254.16.34"
+port = 8888
+port2 = 1234
+
+cap_video = cv2.VideoCapture(0)
+widhtScreen = 640
+heightScreen = 480
+
+zamanlistesi = []
+zamanlistesi2 = []
+
+# haberlesme video udp baglanma
+iha_haber2 = haberlesme.client_UDP()
+udp2 = iha_haber2.baglan(host, port2)
+print("baglandiudp")
+
+# haberlesme veri tcp baglanma
+iha_tele = haberlesme.client_TCP()
+iha_tele.baglan(host, port)
+print("baglandi")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--connect', default='/dev/ttyACM0')
+args = parser.parse_args()
+connection_string = args.connect
+# -- Tuygun Bağlantı
+iha = path.Plane(connection_string)
+
+def mavlinkrouter():
+    os.system('mavproxy.py --master=/dev/ttyACM0,57600 --out=udpbcast:169.254.102.128:14550') #Yer Istasyonu IP
+  
+
+circlesayac = []
+loiter = 0
+#ilk_enlem = iha.pos_lat
+#ilk_boylam = iha.pos_lon
+
+
+datasayac = time.time()
+g = time.time()
+sure = time.time()
+iha_tele.s.settimeout(0.001)
+x_right,x_left,y_up,y_down=0,0,0,0
+
+
+def send_video():
+    global x_right,x_left,y_up,y_down,iha_haber2 
+    #frame = np.zeros((640, 480, 3), dtype=np.uint8)
+
+    while True:
+        try:           
+            ret, frame = cap_video.read()
+            frame = cv2.flip(frame, 1)
+            #print("Return: ", ret)
+            #x_right, x_left, y_up, y_down = frame.shape[1] * 0.25, frame.shape[1] * 0.75, frame.shape[0] * 0.10,frame.shape[0] * 0.90
+            if ret:
+                iha_haber2.video_gonder(frame)
+                if cv2.waitKey(1) & 0xFF==ord("q"):
+                    cap_video.release()
+                    cv2.destroyeAllWindows()
+                
+        except ConnectionResetError as e:            
+            print("ihada video koptu1:"+str(e))
+            iha_haber2.s.close()
+            connected=False
+            iha_haber2 = haberlesme.client_UDP()
+            while not connected:
+                try:
+                    iha_haber2.baglan(host, port)
+                    iha_haber2.s.settimeout(0.001)
+                    connected=True
+                except Exception as e:
+                    print("ihada video koptu2:"+str(e))
+                    time.sleep(1)
+                    pass
+        except Exception as e:
+            print("ihada video koptu3:"+str(e))
+
+def recv_udp():
+    try:
+        data = iha_haber2.mesaj_al()
+        pwm_verileri=ast.literal_eval(data)
+        return pwm_verileri
+    except Exception as e:
+        pass
+
+def recv_tcp():
+    try:
+        data = iha_tele.telemetri_al()
+        return json.loads(data)
+    except Exception as e:
+        pass
+
+pool = ThreadPool(processes=3)
+rakip = None
+pool.apply_async(send_video)
+# pool.apply_async(mavlinkrouter)
+first = 0
+qr_gidiyor = False
+kalkista = False
+global speed
+speed = 1500
+
+while True:
+    try:
+        #print("switch  6:",iha.ch6)
+        #print("switch  8:",iha.ch8)
+        #print("---------------------------------------")
+        
+        
+        
+       # print("1:",iha.servo1)
+       # print("2:",iha.servo2)
+       # print("3:",iha.servo3)
+       # print("4:",iha.servo4)
+       #print("switch  6:",iha.ch6)
+       #print("switch 7:",iha.ch7)
+       # print("---------------------------------------")
+        
+        
+   
+        mesaj = iha.mesajlar()
+        mesaj['IHA_otonom'] = 0
+        mod="haberlesmetest"
+        
+        
+        if time.time() - sure > 0.1:
+            sure = time.time()
+            
+            # AUTO
+            if iha.ch6 > 1600 :  # Servo6: High, Servo7: Mid
+                #mesaj['IHA_otonom'] = 1
+                if iha.get_ap_mode() != "AUTO":
+                    iha.set_ap_mode("AUTO")
+            # FBWA
+            if iha.ch6 > 1400 and iha.ch6 < 1600 :  # Servo6: High, Servo7: Mid
+                #mesaj['IHA_otonom'] = 0
+                if iha.get_ap_mode() != "FBWA":
+                    iha.set_ap_mode("FBWA")
+            
+            # RTL
+            if iha.ch6 < 1400 :  # Servo6: High, Servo7: Mid
+                #mesaj['IHA_otonom'] = 1
+                if iha.get_ap_mode() != "RTL":
+                    iha.set_ap_mode("RTL")
+                    
+            mesaj['mod'] = mod
+            iha_tele.telemetri_gonder(mesaj)
+            print(mesaj)
+
+    except ConnectionResetError as e:
+        print("ihada veri koptu1:" + str(e))
+        iha_tele.s.close()
+        connected = False
+        iha_tele = haberlesme.client_TCP()
+        while not connected:
+            try:
+                iha_tele.baglan(host, port)
+                iha_tele.s.settimeout(0.001)
+                connected = True
+            except Exception as e:
+                print("ihada veri koptu2:" + str(e))
+                time.sleep(1)
+                pass
+    except Exception as e:
+        print("ihada veri koptu3:" + str(e))
+
