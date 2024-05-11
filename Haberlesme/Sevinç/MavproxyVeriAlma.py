0869dffb3c5802862fe769a6c4fa9e84aa353b32
sevinc26 <svnckdmr@gmail.com>
2024-05-11 17:51:07 +0300

    zaman verisi düzgün gelecek şekilde ayarlandı

--- Haberlesme/Sevinç/MavproxyVeriAlma.py
+++ Haberlesme/Sevinç/MavproxyVeriAlma.py
@@ -47,70 +47,72 @@ class MAVLink:
             23: "QACRO",
             24: "THERMAL"
         }
-
-    def connect(self, port='tcp:10.80.1.95:14550'):
+#mustafa berkay
+    """def connect(self, port='tcp:10.80.1.95:14550'):
+        self.master = mavutil.mavlink_connection(port)"""
+#sevinç
+    def connect(self, port='tcp:127.0.0.1:14550'):
         self.master = mavutil.mavlink_connection(port)
 
     def veri_kaydetme(self):
-        while True:
+        self.msg = self.master.recv_match(blocking=True)  # Burada mesajı tanımlıyoruz bu mesajlar bize farklı
+        if self.msg.get_type() == 'GPS_RAW_INT':
+            self.enlem = self.msg.lat / 10000000
+            self.boylam = self.msg.lon / 10000000
+        elif self.msg.get_type() == 'GLOBAL_POSITION_INT':
+            self.yukseklik = self.msg.relative_alt / 1000
+        elif self.msg.get_type() == 'VFR_HUD':
+            self.yer_hizi = self.msg.groundspeed
+            self.hava_hizi = self.msg.airspeed
+        elif self.msg.get_type() == 'SYS_STATUS':
+            self.batarya = self.msg.battery_remaining
+        elif self.msg.get_type() == 'ATTITUDE':
+            self.roll = self.msg.roll
+            self.pitch = self.msg.pitch
+            self.yaw = self.msg.yaw
+        elif self.msg.get_type() == 'HEARTBEAT':
+            self.custom_mode = self.msg.custom_mode
+            self.mod = self.mode_mapping.get(self.custom_mode, str(self.custom_mode))
+        elif self.msg.get_type() == 'SYSTEM_TIME':
+            system_time_unix = self.msg.time_unix_usec / 1e6  # Mikrosaniyeden saniyeye çevirme
+            system_time = time.gmtime(system_time_unix)
+            gps_time = time.strftime('%Y-%m-%d %H:%M:%S',
+                                     system_time)  # Saat, dakika ve saniye cinsinden GPS zamanı alır
+            self.saat = system_time.tm_hour
+            self.dakika = system_time.tm_min
+            self.saniye = system_time.tm_sec
+            self.milisaniye = int((system_time_unix % 1000000) / 1000)
 
-            self.msg = self.master.recv_match(blocking=True)  # Burada mesajı tanımlıyoruz bu mesajlar bize farklı
-            if self.msg.get_type() == 'GPS_RAW_INT':
-                self.enlem = self.msg.lat / 10000000
-                self.boylam = self.msg.lon / 10000000
-            elif self.msg.get_type() == 'GLOBAL_POSITION_INT':
-                self.yukseklik = self.msg.relative_alt / 1000
-            elif self.msg.get_type() == 'VFR_HUD':
-                self.yer_hizi = self.msg.groundspeed
-                self.hava_hizi = self.msg.airspeed
-            elif self.msg.get_type() == 'SYS_STATUS':
-                self.batarya = self.msg.battery_remaining
-            elif self.msg.get_type() == 'ATTITUDE':
-                self.roll = self.msg.roll
-                self.pitch = self.msg.pitch
-                self.yaw = self.msg.yaw
-            elif self.msg.get_type() == 'HEARTBEAT':
-                self.custom_mode = self.msg.custom_mode
-                self.mod = self.mode_mapping.get(self.custom_mode, str(self.custom_mode))
-            elif self.msg.get_type() == 'GPS_TIME':
-                self.gps_saat = self.msg.time
-                self.gps_dakika = self.msg.date
-                self.gps_saniye = self.msg.time
-                self.gps_milisaniye = self.msg.time
 
-
-
-            self.telemetri_verisi = {
-                "takim_numarasi": 1,
-                "iha_enlem": float("{:.7f}".format(self.enlem)),
-                "iha_boylam": float("{:.7f}".format(self.boylam)),
-                "iha_irtifa": float("{:.2f}".format(self.yukseklik)),
-                "iha_dikilme": float("{:.2f}".format(self.pitch)),
-                "iha_yonelme": float("{:.2f}".format(self.yaw)),
-                "iha_yatis": float("{:.2f}".format(self.roll)),
-                "iha_hiz": float("{:.2f}".format(self.yer_hizi)),
-                "iha_batarya": self.batarya,
-                "iha_otonom": 0,
-                "iha_kilitlenme": 0,
-                "hedef_merkez_X": 0,
-                "hedef_merkez_Y": 0,
-                "hedef_genislik": 0,
-                "hedef_yukseklik": 0,
-                "gps_saati": {
-                    #Bu düzenlenecektir.
-                    "saat": self.gps_saat,
-                    "dakika": self.gps_dakika,
-                    "saniye": self.gps_saniye,
-                    "milisaniye": self.gps_milisaniye
-                                      },
-                "iha_mode": self.mod,
-            }
-            print(self.telemetri_verisi)
+        self.telemetri_verisi = {
+            "takim_numarasi": 1,
+            "iha_enlem": float("{:.7f}".format(self.enlem)),
+            "iha_boylam": float("{:.7f}".format(self.boylam)),
+            "iha_irtifa": float("{:.2f}".format(self.yukseklik)),
+            "iha_dikilme": float("{:.2f}".format(self.pitch)),
+            "iha_yonelme": float("{:.2f}".format(self.yaw)),
+            "iha_yatis": float("{:.2f}".format(self.roll)),
+            "iha_hiz": float("{:.2f}".format(self.yer_hizi)),
+            "iha_batarya": self.batarya,
+            "iha_otonom": 0,
+            "iha_kilitlenme": 0,
+            "hedef_merkez_X": 0,
+            "hedef_merkez_Y": 0,
+            "hedef_genislik": 0,
+            "hedef_yukseklik": 0,
+            "gps_saati": {
+                "saat": self.saat,
+                "dakika": self.dakika,
+                "saniye": self.saniye,
+                "milisaniye": self.milisaniye
+                                  },
+            "iha_mode": self.mod,
+        }
+        print(self.telemetri_verisi)
 
 try:
     MAVLink = MAVLink()
     MAVLink.connect()
-    time.sleep(3)
     MAVLink.veri_kaydetme()
 except KeyboardInterrupt:
     pass
