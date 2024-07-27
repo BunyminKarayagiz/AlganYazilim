7393472483fe4065f1842ef8c5c798cc1adde614
mbakcan211 <mustafa.akcan.b8@gmail.com>
2024-07-27 15:25:07 +0300

    Raspberry düzenleme

--- Savasan-iha/Mustafa Berkay/Iha_test.py
+++ Savasan-iha/Mustafa Berkay/Iha_test.py
@@ -2,8 +2,8 @@ import argparse
 import json
 import numpy as np
 import path
-
-import time
+    
+import time , datetime
 import threading
 import Client_Tcp
 
@@ -22,6 +22,7 @@ class Iha():
         self.TCP_yonelim=Client_Tcp.Client(host_ip,9002)
         self.TCP_pwm=Client_Tcp.Client(host_ip,9001)
         self.TCP_mod=Client_Tcp.Client(host_ip,9003)
+        self.TCP_kamikaze=Client_Tcp.Client(host_ip,9004)
         self.yönelim_yapılacak_rakip=""
         self.mevcut_mod =""
         self.onceki_mod =""
@@ -58,6 +59,16 @@ class Iha():
                 print("MOD SERVER: BAĞLANDI.")
             except (ConnectionError , Exception) as e:
                 print("MOD SERVER: baglanırken hata: ", e)
+                
+    def kamikaze_sunucusuna_baglan(self):
+        connection=False
+        while not connection:
+            try:
+                self.TCP_kamikaze.connect_to_server()
+                connection=True
+                print("MOD SERVER: BAĞLANDI.")
+            except (ConnectionError , Exception) as e:
+                print("MOD SERVER: baglanırken hata: ", e)
 
     def IHA_MissionPlanner_Connect(self, tcp_port):
         parser = argparse.ArgumentParser()
@@ -86,6 +97,8 @@ class Iha():
     def sunuculara_baglan(self):
         self.Yonelim_sunucusuna_baglan()
         self.PWM_sunucusuna_baglan()
+        self.kamikaze_sunucusuna_baglan()
+        
 
 
     #KİLİTLENME FONKSİYONLARI
@@ -136,21 +149,25 @@ class Iha():
                     print("QR-KONUM: Veri çekilirken hata :", e)
             
     def kamikaze_yönelim(self,iha_path): #! KRITIK, Dalış anında modu değişimi sorun olabilir..
-        time.sleep(0.3)
+        #time.sleep(0.3)
         if self.mevcut_mod != "kamikaze" :
                     print("KAMIKAZE -> BEKLEME MODU")
                     self.kamikaze_release_event.wait()
                     print("KAMIKAZE -> AKTIF")
                     self.kamikaze_release_event.clear()
         
-        is_qr_available= False
-        try:
-            while not is_qr_available:
-                is_qr_available=self.qr_konum_al() #TODO Code-Blocking line..
-                print("QRKONUM ALINDI\nQRKONUM ALINDI\nQRKONUM ALINDI\nQRKONUM ALINDI\nQRKONUM ALINDI\nQRKONUM ALINDI\nQRKONUM ALINDI\n")
-        except Exception as e:
-            print("KAMIKAZE :QR-Konum gg ->",e)
+        # is_qr_available= False
+        # try:
+            # while not is_qr_available:
+                # is_qr_available=self.qr_konum_al() #TODO Code-Blocking line..
+                # print("QRKONUM ALINDI\nQRKONUM ALINDI\nQRKONUM ALINDI\nQRKONUM ALINDI\nQRKONUM ALINDI\nQRKONUM ALINDI\nQRKONUM ALINDI\n")
+        # except Exception as e:
+            # print("KAMIKAZE :QR-Konum gg ->",e)
 
+        #Manuel verilen qr self.yönelim_yapılacak_rakip = {
+        qr_enlem = 0
+        qr_boylam = 0
+                
 
         try:
             qr_gidiyor=False
@@ -163,7 +180,7 @@ class Iha():
                     self.kamikaze_release_event.clear()
 
                     
-                qr_enlem, qr_boylam = json.loads(self.yönelim_yapılacak_rakip)["qrEnlem"], json.loads(self.yönelim_yapılacak_rakip)["qrBoylam"]
+                #qr_enlem, qr_boylam = json.loads(self.yönelim_yapılacak_rakip)["qrEnlem"], json.loads(self.yönelim_yapılacak_rakip)["qrBoylam"]
                 qr_mesafe = vincenty([iha_path.pos_lat, iha_path.pos_lon], [qr_enlem, qr_boylam], 100)
                 print("QR MESAFE", qr_mesafe)
                 if not qr_gidiyor and not kalkista and qr_mesafe > 0.15:  # and iha.pos_alt_rel > 100:
@@ -177,6 +194,8 @@ class Iha():
                 if qr_mesafe < 0.08 and qr_gidiyor:  # 150 metre
                     if iha_path.get_ap_mode() != "FBWA":
                         iha_path.set_ap_mode("FBWA")
+                        kamikaze_start= datetime.datetime.now()
+                        self.TCP_kamikaze.send_message_to_server(kamikaze_start)
                     iha_path.set_rc_channel(1, 1500)  # Channel 1 is for Roll Input,
                     iha_path.set_rc_channel(2, 1100)  # Channel 2 is for Pitch Input,
                     iha_path.set_rc_channel(3, 1100)  # Channel 3 is for Throttle Input,
@@ -203,8 +222,8 @@ class Iha():
 
 if __name__ == '__main__':
 
-    iha_obj = Iha("10.80.1.60") #UÇAK İÇİN VERİLEN İP DEĞİŞTİRİLECEK. 10.0.0.236
-    iha_path = iha_obj.IHA_MissionPlanner_Connect(5762) #UÇAK İÇİN VERİLEN FONKSİYON RASPBERRY_CONNECT OLACAK.
+    iha_obj = Iha("10.80.1.114") #UÇAK İÇİN VERİLEN İP DEĞİŞTİRİLECEK. 10.0.0.236
+    iha_path = iha_obj.IHA_Raspberry_Connect() #UÇAK İÇİN VERİLEN FONKSİYON RASPBERRY_CONNECT OLACAK.
 
     print("2 Sn bekleniyor...")
     time.sleep(2) #Tüm Bağlantıların Yerine Oturması için 2 sn bekleniyor
@@ -224,7 +243,32 @@ if __name__ == '__main__':
 
     while True:
         time.sleep(0.5)
-        if (iha_path.servo6 > 1600 and iha_path.servo7 < 1400) or DEBUG=="DEBUG_QR":  # ch6: High, ch8: LOW
+        if (iha_path.ch6 > 1600 and iha_path.ch8 > 1600):  # High High
+            iha_obj.mod = "AUTO" 
+            iha_obj.TCP_mod.send_message_to_server(iha_obj.mod)
+            iha_obj.onceki_mod = "AUTO"
+            if iha_path.get_ap_mode()!="AUTO":
+                iha_path.set_ap_mode("AUTO")
+            print("SELECTED MOD : AUTO")
+                
+        if ((iha_path.ch6 >= 1400 and iha_path.ch6 <= 1600) and iha_path.ch8 > 1600):  # Mid High
+            iha_obj.mod = "FBWA" 
+            iha_obj.TCP_mod.send_message_to_server(iha_obj.mod)
+            iha_obj.onceki_mod = "FBWA"
+            if iha_path.get_ap_mode()!="FBWA":
+                iha_path.set_ap_mode("FBWA")
+            print("SELECTED MOD : FBWA")
+        
+        if (iha_path.ch6 < 1400 and iha_path.ch8 > 1600):  # LOW High
+            iha_obj.mod = "RTL" 
+            iha_obj.TCP_mod.send_message_to_server(iha_obj.mod)
+            iha_obj.onceki_mod = "RTL"
+            if iha_path.get_ap_mode()!="RTL":
+                iha_path.set_ap_mode("RTL")
+            print("SELECTED MOD : RTL")
+        
+            
+        if (iha_path.ch6 > 1600 and iha_path.ch8 < 1400) or DEBUG=="DEBUG_QR":  # ch6: High, ch8: LOW
             iha_obj.mod = "kamikaze"
             iha_obj.TCP_mod.send_message_to_server(iha_obj.mod)
             iha_obj.kamikaze_release_event.set()
@@ -236,14 +280,15 @@ if __name__ == '__main__':
                     print("DEBUG MOD ON...\n\n")
                     time.sleep(9999)
 
-        if (iha_path.servo6 >= 1600 and iha_path.servo7 >= 1600) or DEBUG=="DEBUG_LOCK":  # ch6: High, ch8: High
+        if (iha_path.ch6 >= 1600 and (iha_path.ch8 > 1400 and iha_path.ch8 < 1600)) or DEBUG=="DEBUG_LOCK":  # ch6: High, ch8: Mid
             iha_obj.mod = "kilitlenme"
             iha_obj.TCP_mod.send_message_to_server(iha_obj.mod)
             iha_obj.yönelim_release_event.set()
             iha_obj.onceki_mod = "kilitlenme"
+            if iha_path.get_ap_mode()!="FBWA":
+                iha_path.set_ap_mode("FBWA")
             print("SELECTED MOD : KILITLENME")
-
-            
+                        
             if DEBUG == "DEBUG_kilitlenme":
                 while True:
                     print("DEBUG MOD ON...\n\n")
---
+++ Savasan-iha/Mustafa Berkay/video_stream.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Provided IP: $1"
+
+rpicam-vid -t 0 --inline -o "udp://$1:5555"
