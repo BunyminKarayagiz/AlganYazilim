3d0b95c0a3fcfb13584b55b54e76304fb4d85c2f
Bünyamin Karayağız <bunyamin4152@gmail.com>
2024-08-15 18:53:11 +0300

    yönelim sunucu

---
+++ Savasan-iha/Mustafa Berkay/Testing/Yonelim_Arayüzü/rakip.py
@@ -0,0 +1,35 @@
+import path
+import sunucuislemleri
+import argparse
+import time
+
+ana_sunucu=sunucuislemleri.sunucuApi("http://127.0.0.1:5000")
+
+ana_sunucu.sunucuya_giris("algan2","1234")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--connect', default=f'tcp:127.0.0.1:5772')
+args = parser.parse_args()
+connection_string = args.connect
+rakip=path.Plane(connection_string)
+
+time.sleep(2)
+
+while True:
+
+    
+    rakip_veri = {
+                "takim_numarasi": 2,
+                "iha_enlem": rakip.pos_lat,
+                "iha_boylam": rakip.pos_lon,
+                "iha_irtifa": rakip.pos_alt_rel,
+                "iha_dikilme":rakip.att_pitch_deg,
+                "iha_yonelme": rakip.att_heading_deg,
+                "iha_yatis": rakip.att_roll_deg,
+                "iha_hizi": rakip.airspeed,
+                "zaman_farki": 500}
+
+
+    status , rakip_telemetri= ana_sunucu.sunucuya_postala(rakip_veri)
+    print(rakip_telemetri)
+    time.sleep(1)
